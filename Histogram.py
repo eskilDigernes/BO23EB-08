74c46e8c96f25ffdeede73df08f7d33ceecfa63a
eskilDigernes <112086034+eskilDigernes@users.noreply.github.com>
2023-01-31 13:03:54 +0100

    Added Histogram

---
+++ Histogram.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+path = 'resources/redspots/iPhone/rediPhone1.png'
+img = cv2.imread(path)
+cv2.imshow('Original', img)
+
+colour = ('b','g','r')          # create a tuple of colours
+for i,col in enumerate(colour):
+    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+plt.title('Histogram of red hotspots')
+plt.grid(True, linestyle='--')
+plt.show()
+
+cv2.waitKey(0)
+
+
+
+
+
+
