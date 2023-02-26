e7cedf5fc8df3c20aa1173341146f9177ee58177
eskilDigernes <112086034+eskilDigernes@users.noreply.github.com>
2023-02-26 20:51:55 +0100

    added one megapixel images to image_paths list

--- Object_Detection_Test.py
+++ Object_Detection_Test.py
@@ -1,17 +1,28 @@
 import torch
 
-# Model
+# Models
 model_asbjorn = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
 model_stian = torch.hub.load('ultralytics/yolov5', 'custom', 'weight_all.pt')
 
-# Images
+# List of images
 image_paths = [
-    'Resources\Full_Image_set\IMG_3258.jpg',
-    'Resources\Full_Image_set\IMG_3273.jpg',
-    'Resources\Full_Image_set\GOPR0125.jpg',
-    'Resources\Full_Image_set\IMG_7438.jpg',
-    'Resources\Full_Image_set\IMG_3282.jpg',
-    'Resources\Full_Image_set\IMG_3284.jpg',
+
+#Full resolution
+'Resources\Full_Image_set\IMG_3258.jpg',
+'Resources\Full_Image_set\IMG_3273.jpg',
+'Resources\Full_Image_set\GOPR0125.jpg',
+'Resources\Full_Image_set\IMG_7438.jpg',
+'Resources\Full_Image_set\IMG_3282.jpg',
+'Resources\Full_Image_set\IMG_3284.jpg',
+
+# 1 mega pixel
+'Resources\Full_Image_set_resized\GOPR0117.jpg',
+'Resources\Full_Image_set_resized\GOPR0125.jpg',
+'Resources\Full_Image_set_resized\IMG_3269.jpg',
+'Resources\Full_Image_set_resized\IMG_3273.jpg',
+'Resources\Full_Image_set_resized\IMG_7428.jpg',
+'Resources\Full_Image_set_resized\IMG_7437.jpg',
+'Resources\Full_Image_set_resized\IMG_7438.jpg',
 ]
 
 # Show results ASBJORN
