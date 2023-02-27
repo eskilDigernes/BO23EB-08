d2cc8fdd471338ad6759408f5002dab551d720cf
AsbjornTje <asbjorn_tjensvold@hotmail.com>
2023-02-27 11:28:25 +0100

    NICK

--- Object_Detection_Test.py
+++ Object_Detection_Test.py
@@ -1,5 +1,9 @@
 import torch
+from PIL import Image
+import glob
+from itertools import islice
 
+<<<<<<< Updated upstream
 # Models
 model_asbjorn = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
 model_stian = torch.hub.load('ultralytics/yolov5', 'custom', 'weight_all.pt')
@@ -34,3 +38,20 @@ for img in image_paths:
 for img in image_paths:
     results = model_stian(img)
     results.show()
+=======
+image_list = []
+for filename in glob.glob('Resources\Full_Image_set/*.jpg'): #assuming jpg
+    im=Image.open(filename)
+    image_list.append(im)
+    
+# Model
+model_asbjorn = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
+model_stian = torch.hub.load('ultralytics/yolov5', 'custom', 'weight_all.pt')
+
+iterator = islice(image_list, 3)
+for img in iterator:
+    results = model_asbjorn(img)
+    results.show()
+    results.save('Resources\Results')   
+
+>>>>>>> Stashed changes
